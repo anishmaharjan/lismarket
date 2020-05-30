b4caf450e5db935404afd3c51fca820ec1c6de33
Anish Maharjan <anish@lawpath.com.au>
2020-05-30 22:15:56 +1000

    feat: fonts update

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -15,61 +15,79 @@ const SignIn = props => {
 
   const [form, setForm] = useState({});
 
+  const Text = styled.Text({
+    // fontFamily:
+    // Platform.OS === 'ios' ? 'Questrial-Regular' : 'some_filename.ttf',
+  });
+
   const submitForm = () => {
-    dispatch(signIn(form, (userData) => {
-      dispatch(getUserApi(userData.email))
-    }));
+    dispatch(
+      signIn(form, userData => {
+        dispatch(getUserApi(userData.email));
+      }),
+    );
   };
 
   return (
-      <Container>
-        <View style={css`
-        margin-top: 30px;`}>
-          <Text style={css`
-          font-size: 24px; 
-          color: ${gas.text};
-          padding: 10px 20px`}>Sign in to your account</Text>
-          <Input
-              placeholder='Username'
-              onChangeText={val => setForm(prev => ({...prev, username: val}))}
-              autoCapitalize='none'
-              containerStyle={css`
+    <Container>
+      <View
+        style={css`
+          margin-top: 30px;
+        `}>
+        <Text
+          style={css`
+            font-size: 24px;
+            color: ${gas.text};
             padding: 10px 20px;
-            `}
-          />
-          <Input
-              placeholder='Password'
-              onChangeText={val => setForm(prev => ({...prev, password: val}))}
-              secureTextEntry={true}
-              containerStyle={css`padding: 10px 20px;`}
-          />
-          <Button
-              title="Sign in"
-              style={css`
-              padding: 10px 20px;
-            `}
-              buttonStyle={{
-                backgroundColor: gas.primary,
-              }}
-              onPress={submitForm}
-              loading={fetchingSignIn}
-          />
-          <Button
-              title="- or sign up -"
-              type="clear"
-              titleStyle={{
-                color: gas.primary,
-              }}
-              onPress={() => navigation.navigate('SignUpScreen')}
-          />
-        </View>
-      </Container>
+          `}>
+          Sign in to your account
+        </Text>
+        <Input
+          placeholder="Username"
+          onChangeText={val => setForm(prev => ({...prev, username: val}))}
+          autoCapitalize="none"
+          containerStyle={css`
+            padding: 10px 20px;
+          `}
+        />
+        <Input
+          placeholder="Password"
+          onChangeText={val => setForm(prev => ({...prev, password: val}))}
+          secureTextEntry={true}
+          containerStyle={css`
+            padding: 10px 20px;
+          `}
+        />
+        <Button
+          title="Sign in"
+          style={css`
+            padding: 10px 20px;
+          `}
+          buttonStyle={{
+            backgroundColor: gas.primary,
+          }}
+          onPress={submitForm}
+          loading={fetchingSignIn}
+        />
+        <Button
+          title="- or sign up -"
+          type="clear"
+          titleStyle={{
+            color: gas.primary,
+          }}
+          onPress={() => navigation.navigate('SignUpScreen')}
+        />
+      </View>
+    </Container>
   );
 };
-export default connect(state => ({
-  fetchingSignIn: state.auth.fetchingSignIn,
-}), dispatch => ({
-  dispatch,
-  signIn,
-  getUserApi
-}))(SignIn);
+export default connect(
+  state => ({
+    fetchingSignIn: state.auth.fetchingSignIn,
+  }),
+  dispatch => ({
+    dispatch,
+    signIn,
+    getUserApi,
+  }),
+)(SignIn);
